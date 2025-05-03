8b63e292ff0f655ce03f3d9a55c96949412443fa
Cameron Lucas <cameronlucasaim@gmail.com>
2025-05-03 16:03:02 -0600

    adds vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -60,6 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-P57SQ55K" />
+      <Analytics />
       <body className={`${roboto.variable} antialiased`}>{children}</body>
     </html>
   );
