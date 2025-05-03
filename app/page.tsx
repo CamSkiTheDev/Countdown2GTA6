a86dbd87eb07b6e2cec6165a8395af85c20eaa9e
Cameron Lucas <cameronlucasaim@gmail.com>
2025-05-02 23:39:20 -0600

    base project

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,119 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useMemo, useState } from "react";
+
+type RemainingTime = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const relealseDate = new Date("2026-05-23T00:00:00Z");
+  const initRemainingTime = useMemo<RemainingTime>(() => {
+    const now = new Date();
+    const timeDiff = relealseDate.getTime() - now.getTime();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return {
+      days: Math.floor(timeDiff / (1000 * 3600 * 24)),
+      hours: Math.floor((timeDiff % (1000 * 3600 * 24)) / (1000 * 3600)),
+      minutes: Math.floor((timeDiff % (1000 * 3600)) / (1000 * 60)),
+      seconds: Math.floor((timeDiff % (1000 * 60)) / 1000),
+    };
+  }, []);
+
+  const [remainingTime, setRemainingTime] =
+    useState<RemainingTime>(initRemainingTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const timeDiff = relealseDate.getTime() - now.getTime();
+
+      setRemainingTime({
+        days: Math.floor(timeDiff / (1000 * 3600 * 24)),
+        hours: Math.floor((timeDiff % (1000 * 3600 * 24)) / (1000 * 3600)),
+        minutes: Math.floor((timeDiff % (1000 * 3600)) / (1000 * 60)),
+        seconds: Math.floor((timeDiff % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+      setRemainingTime(initRemainingTime);
+    };
+  }, []);
+  return (
+    <div className="flex justify-center items-center w-full h-full bg-[url('/bg.jpg')] bg-cover bg-center">
+      <div className="flex flex-col items-center justify-center w-full h-full min-h-screen bg-black/50 space-y-8 p-4">
+        {/* For TSX uncomment the commented types below */}
+        <div className="font-roboto grid grid-flow-col gap-5 text-center auto-cols-max">
+          <div className="flex flex-col">
+            <span className="countdown font-mono text-4xl md:text-6xl lg:text-8xl">
+              <span
+                style={
+                  { "--value": remainingTime.days } /* as React.CSSProperties */
+                }
+                aria-live="polite"
+                aria-label={remainingTime.days}
+              >
+                {remainingTime.days}
+              </span>
+            </span>
+            days
+          </div>
+          <div className="flex flex-col">
+            <span className="countdown font-mono text-4xl md:text-6xl lg:text-8xl">
+              <span
+                style={
+                  {
+                    "--value": remainingTime.hours,
+                  } /* as React.CSSProperties */
+                }
+                aria-live="polite"
+                aria-label={remainingTime.hours}
+              >
+                {remainingTime.hours}
+              </span>
+            </span>
+            hours
+          </div>
+          <div className="flex flex-col">
+            <span className="countdown font-mono text-4xl md:text-6xl lg:text-8xl">
+              <span
+                style={
+                  {
+                    "--value": remainingTime.minutes,
+                  } /* as React.CSSProperties */
+                }
+                aria-live="polite"
+                aria-label={remainingTime.minutes}
+              >
+                {remainingTime.minutes}
+              </span>
+            </span>
+            min
+          </div>
+          <div className="flex flex-col">
+            <span className="countdown font-mono text-4xl md:text-6xl lg:text-8xl">
+              <span
+                style={
+                  {
+                    "--value": remainingTime.seconds,
+                  } /* as React.CSSProperties */
+                }
+                aria-live="polite"
+                aria-label={remainingTime.seconds}
+              >
+                {remainingTime.seconds}
+              </span>
+            </span>
+            sec
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h3 className="font-roboto font-black text-lg md:text-2xl lg:text-4xl text-shadow-xl opacity-90">
+          Until May 23rd, 2026
+        </h3>
+      </div>
     </div>
   );
 }
